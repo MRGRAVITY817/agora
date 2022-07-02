c2fe5a6a240b48764e323440e82c88975f54c33c
Hoon Wee <mrgravity817@gmail.com>
2022-07-02 11:04:02 +0900

    feat(handler): add handler in about page

---
+++ routes/about.tsx
@@ -0,0 +1,20 @@
+/** @jsx h */
+import { h } from "preact"
+import { Handlers } from "$fresh/server.ts"
+
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    const res = await ctx.render()
+    res.headers.set("X-Custom-Header", "Hello")
+    return res
+  },
+}
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>This is about page</p>
+    </main>
+  )
+}
